bb31df630574ea54c3adbe585eca797da431a92c
aaveryy <averyelizabeth7299@gmail.com>
2023-12-04 20:13:02 +0000

    working health bar kinda!
    no enemies yet so i just make player take damage from hitting space bar to test, but it works ! and fun cool gradient too

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,18 +6,24 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 200;
     public int playerHealth;
+    public HealthBar healthBar;
     
     void Start()
     {
         playerHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void PlayerTakeDamage(int damage)
     {
     playerHealth = playerHealth - damage;
+    healthBar.SetHealth(playerHealth);
     } 
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            PlayerTakeDamage(20);
+        }
         if(playerHealth < 1){ 
         Destroy(this.gameObject);
         }
